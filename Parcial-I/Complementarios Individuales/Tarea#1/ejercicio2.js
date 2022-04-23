4f2822c0b9616d5e7ae6d620748603a98aceca50
EberAlarcon <92482287+EberAlarcon@users.noreply.github.com>
2022-04-23 15:45:25 -0500

    Add files via upload

---
+++ Parcial-I/Complementarios Individuales/Tarea#1/ejercicio2.js
@@ -0,0 +1,18 @@
+//Crear un objeto Comida que tenga como parametros: nombre, ingredientes, tipo (ensalada, plato fuerte
+//entrada, etc) y cualquier otro atributo que considere importante para su diseño.)
+let comida = {
+    Nombre: 'Caldo de pollo:',
+    Ingredientes: [
+        '1 gallina criolla',
+        '2 ½ litros de agua fría',
+        '1 cebolla paiteña partida en 2',
+        '1 rama cebolla blanca picada',
+        '1 pimiento partido en 2',
+        '4 dientes de ajo picados',
+        '1 rama de culantro picados muy finamente',
+        '1 yuca mediana pelada y cortada en trozos',
+        'Orégano, comino y sal al gusto'],
+    Tipo: 'Plato Fuerte', 
+    Cocina: 'Ecuatoriana'
+}
+console.log(comida);
