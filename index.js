8ad7503800abf81ab0244faea0d2e037537542cf
EberAlarcon <alarconeber2001@gmail.com>
2022-05-07 10:24:54 -0500

    master

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const conexion= "mongodb+srv://eber:aplicaciones@cluster0.lhuw2.mongodb.net/Ejem
 //funcion asincronica colocando varias constantes para establecer la conexion 
 ( async ()=>{
     const estadoConexion = await mongoose.connect(conexion);
-    //se crea como un modelo de una base de dato como entidad el usuario y un atributo de nombre como string
+    //se crea como un modelo fdfd de una base de dato como entidad el usuario y un atributo de nombre como string
     const Usuario =  mongoose.model("Usuario", { nombre: String } );
     //establezco un dato en el atributo de nombre
     const usuario1=  new Usuario({nombre:"Prueba sexto A"});
