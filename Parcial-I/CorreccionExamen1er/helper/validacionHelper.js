b02f2d5da961c8d8997f8e96113772deeb17593f
EberAlarcon <alarconeber2001@gmail.com>
2022-06-02 18:18:42 -0500

    main

--- Parcial-I/CorreccionExamen1er/helper/validacionHelper.js
+++ Parcial-I/CorreccionExamen1er/helper/validacionHelper.js
@@ -8,11 +8,7 @@ const ValidarResultado = (req, res, next) => {
         return next(); //Si todo esta bien en las validaciones sigue su curso la app
 
     }catch(err){
-
         res.status(403).send({errors:"No existe" }) //Si hay un error nos retorna el error
-
     }
-
 }
-
 module.exports = {ValidarResultado}
--- Parcial-I/CorreccionExamen1er/index.js
+++ Parcial-I/CorreccionExamen1er/index.js
@@ -1,8 +1,8 @@
-//localhost:5000/api/uno
+//localhost:3000/api/uno
 const express = require('express'); //REQUERIMOS EXPRESS
 
-//CONFIG PUERTO
-const port = process.env.PORT || 5000;
+//CONFIGURAR PUERTO
+const port = process.env.PORT || 3000;
 
 //RUTAS
 const computadora_route = require('./routes/Usuario')
@@ -10,7 +10,7 @@ const computadora_route = require('./routes/Usuario')
 //INICIALIZAR EXPRESS
 const app = express();
 
-//CONFIG SERVER
+//CONFIGURAR SERVER
 app.use(express.json());
 
 //USO DE RUTA
@@ -27,7 +27,7 @@ app.use('/api',(error, req, res, next) => {
     });
 });
 
-//CONFIG APP
+//CONFIGURAR APP
 app.listen(port, function() {
     console.log("API Rest corriendo en el puerto: " + port);
     console.log(" " + " " + " " +" " + " " + " " +" " + " " + " " + "CONEXION CORRECTA");
