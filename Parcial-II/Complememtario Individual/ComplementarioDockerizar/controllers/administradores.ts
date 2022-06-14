cbd08bfa742e6f1436ba17acc19161052e1b69dc
EberAlarcon <alarconeber2001@gmail.com>
2022-06-14 15:32:28 -0500

    main

---
+++ Parcial-II/Complememtario Individual/ComplementarioDockerizar/controllers/administradores.ts
@@ -0,0 +1,11 @@
+//impotamos la varaible definida de la carpeta de models y tambien la variable de la carpeta de interfaces
+import {Administrador} from '../models'
+import { IAdministradores } from '../interfaces'
+
+
+
+const Obtener_Administrador = async(req:Reques, res: Response) => {
+    const {id}= req.
+
+
+}
