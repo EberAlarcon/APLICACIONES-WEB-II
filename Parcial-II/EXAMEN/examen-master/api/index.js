b5e0a54276e43efb63f8e3c7505e5705147eab17
EberAlarcon <alarconeber2001@gmail.com>
2022-07-28 14:43:24 -0500

    EXAMEN

---
+++ Parcial-II/EXAMEN/examen-master/api/index.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const ruta = express.Router()
+const puerto = 2500
+
+//Consultar
+let apuestas = []
+app.use(cors()).use(express.json())
+
+ruta.get('/', (req, res) => {
+    res.status(200).send(apuestas)
+})
+
+ruta.get('/:id', (req, res) => {
+    const id = req.params.id
+    const apuesta = apuestas.find((apuesta) => apuesta.codigo == id)
+    if (apuesta) {
+        res.status(200).send(apuesta)
+    } else {
+        res.status(404).send('No se encontró la apuesta')
+    }
+})
+
+//Insertar
+ruta.post('/', (req, res) => {
+    const { body } = req
+    if (apuestas.filter((c) => c.codigo == body.codigo).length > 0) {
+        return res.status(400).send({
+            message: 'El codigo ya existe',
+            response: body,
+        })
+    }
+    apuestas.push(body)
+    res.status(201).send({
+        message: 'El dato se inserto correctamente',
+        response: body,
+    })
+})
+
+//Update
+ruta.put('/:id', (req, res) => {
+    const { id } = req.params
+    const { body } = req
+    if (apuestas.filter((c) => c.codigo == id).length == 0) {
+        return res.status(400).send({
+            message: 'No se encuentra la apuesta que desea modificar',
+        })
+    }
+    let comida = apuestas.filter((c) => c.codigo == id)[0]
+    comida.codigo = body.codigo
+    comida.fecha = body.fecha
+    comida.evento = body.evento
+    comida.resultado = body.resultado
+    comida.valor_apostado = body.valor_apostado
+    if (comida.estado === 'apostado') {
+        comida.estado = body.estado
+    } else {
+        comida.estado = 'apostado'
+    }
+    comida.estado = body.estado
+    res.status(200).send({
+        message: 'Dato modificado con exito',
+        response: comida,
+    })
+})
+
+//Delete
+ruta.delete('/:id', (req, res) => {
+    const { id } = req.params
+    if (apuestas) {
+        apuestas.filter((c) => c.codigo !== id)
+        res.status(200).send({
+            message: 'Dato eliminado con exito',
+        })
+    } else {
+        return res.status(400).send({
+            message: 'No se encuentra la apuesta que desea eliminar',
+        })
+    }
+})
+
+app.use('/', ruta)
+
+app.listen(puerto, () => {
+    console.log(`Servidor Ejecutado en puerto: ${puerto}`)
+})
---
+++ Parcial-II/EXAMEN/examen-master/csr/src/main.ts
@@ -0,0 +1,48 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+  <label>Estado </label>
+    <input type="text" id="estado" />
+    <button id="boton">Cambiar estado</button>
+  </div>
+  <div id="cuerpo"></div>
+`
+const boton = document.querySelector<HTMLButtonElement>('#boton')!
+const estado = document.querySelector<HTMLInputElement>('#estado')!
+const cuerpo = document.querySelector<HTMLDivElement>('#cuerpo')!
+import axios from 'axios'
+
+const http_Axios = axios.create({
+    baseURL: 'http://localhost:2500',
+})
+
+boton.addEventListener('click', async () => {
+    const Estado = estado.value
+    const { data } = await http_Axios.get(`/`)
+    const tabla = document.createElement('table')
+    tabla.id = 'tabla'
+    tabla.border = '1'
+    tabla.align = 'center'
+
+    for (let apuesta of data) {
+        const row = tabla.insertRow()
+        const celda = row.insertCell()
+
+        celda.innerHTML = ` <button class="boton" value="${apuesta.codigo}">${apuesta.evento}</button> `
+        const celda2 = row.insertCell()
+        celda2.innerHTML = `${apuesta.estado}`
+    }
+
+    cuerpo.innerHTML = ''
+    cuerpo.appendChild(tabla)
+    document.querySelectorAll('.boton').forEach((element: Element) => {
+        ;(element as HTMLButtonElement).addEventListener('click', async () => {
+            const { data } = await http_Axios.get(
+                `/${(element as HTMLButtonElement).value}`,
+            )
+            console.log(data)
+            estado.value = data.estado
+        })
+    })
+})
